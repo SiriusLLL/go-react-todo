fad7fade028a2e1902051a8ef14e2328d5d0d44d
Lh <lihao1017@qq.com>
2023-07-17 14:41:39 +0800

    finish To-Do-List.js

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -15,7 +15,124 @@ class TodoList extends Component {
     }
     componentDidMount() {
         this.getTask();
+    };
+
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    onSubmit = () => {
+        let { task } = this.state;
+
+        if (task) {
+            axios.post(endpoint + "/api/tasks",
+                { task, },
+                {
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                }
+            ).then((res) => {
+                this.getTask();
+                this.setState({
+                    task: "",
+                });
+                console.log(res);
+            });
+        }
+    };
+
+    getTask = () => {
+        axios.get(endpoint + "/api/task").then((res) => {
+            if (res.data) {
+                this.setState({
+                    items: res.data.map((item) => {
+                        let color = "yellow";
+                        let style = {
+                            wordWrap: "break-word"
+                        };
+                        if (item.status) {
+                            color = "green"
+                            style["textDecorationLine"] = "line-through";
+                        }
+
+                        return (
+                            <Card key={item._id} color={color} fluid className="rough">
+                                <Card.Content>
+                                    <Card.Header textAlign="left">
+                                        <div style={style}>{item.task}</div>
+                                    </Card.Header>
+                                    <Card.Meta textAlign="left">
+                                        <Icon
+                                            name="check circle"
+                                            color="blue"
+                                            onClick={() => this.updateTask(item._id)}
+                                        />
+                                        <span style={{ paddingRight: 10 }}>Done</span>
+
+                                        <Icon
+                                            name="check circle"
+                                            color="yellow"
+                                            onClick={() => this.undoTask(item._id)}
+                                        />
+                                        <span style={{ paddingRight: 10 }}>Undo</span>
+
+                                        <Icon
+                                            name="delete"
+                                            color="red"
+                                            onClick={() => this.deleteTask(item._id)}
+                                        />
+                                        <span style={{ paddingRight: 10 }}>Delete</span>
+
+
+                                    </Card.Meta>
+                                </Card.Content>
+                            </Card>
+                        );
+                    }),
+                });
+            } else {
+                this.setState({
+                    items: [],
+                });
+            }
+        });
+    };
+
+    updateTask = (id) => {
+        axios.put(endpoint + "/api/tasks/" + id, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }).then((res) => {
+            console.log(res);
+            this.getTask();
+        });
+    };
+
+    undoTask = (id) => {
+        axios.put(endpoint + "/api/undoTask/" + id, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }).then((res) => {
+            console.log(res);
+            this.getTask();
+        });
     }
+
+    deleteTask = (id) => {
+        axios.delete(endpoint + "/api/deleteTasks/" + id, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }).then((res) => {
+            console.log(res);
+            this.getTask();
+        });
+    };
 
     render() {
         return (
@@ -32,10 +149,14 @@ class TodoList extends Component {
                             name="task"
                             onChange={this.onChange}
                             value={this.state.task}
-                            fuild
+                            fluid
                             placeholder="Create a task"
                         />
+                        {/* <Button>Create Task </Button> */}
                     </Form>
+                </div>
+                <div className="row">
+                    <Card.Group>{this.state.items}</Card.Group>
                 </div>
             </div>
         );
